1e9dbf3cd6fcde00ae0afc477b43efdebad0964a
lorensdima <laurencedimalanta@gmail.com>
2024-06-19 22:20:58 +0800

    update conv controller to return hashmap

--- src/main/java/com/mycheck/lchecksum/controller/ConversionController.java
+++ src/main/java/com/mycheck/lchecksum/controller/ConversionController.java
@@ -1,23 +1,30 @@
 package com.mycheck.lchecksum.controller;
+
+import java.util.HashMap;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.mycheck.lchecksum.model.RawJson;
+import com.mycheck.lchecksum.service.ConversionService;
 
 @RestController
 public class ConversionController {
+
+    @Autowired
+    private ConversionService conversionService;
     
     @PostMapping("/convert")
-    public String receiveExampleObject(@RequestBody RawJson obj) {
-        String jsonString = "";
-        try {
-            jsonString = obj.toJsonString();
-            System.out.println("JSON String: " + jsonString);
-        } catch (JsonProcessingException e) {
-            e.printStackTrace();
-        } 
-        return "Received: " + jsonString;
+    public HashMap<String, Object> receiveExampleObject(@RequestBody RawJson obj) {
+        HashMap<String, Object> jsonMap = new HashMap<>();
+        jsonMap = obj.toHashMap();
+
+        String lcheckSumVal = conversionService.computeCheckSum(jsonMap);
+        jsonMap.put("checksum", lcheckSumVal);
+        
+        return jsonMap;
     }
 }
