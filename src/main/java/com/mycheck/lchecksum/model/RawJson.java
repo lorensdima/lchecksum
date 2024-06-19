8a55fedbcb085005d06de8c77f3363f49a1a1e65
lorensdima <laurencedimalanta@gmail.com>
2024-06-19 22:20:29 +0800

    update json model to convert data into hashmap

--- src/main/java/com/mycheck/lchecksum/model/RawJson.java
+++ src/main/java/com/mycheck/lchecksum/model/RawJson.java
@@ -1,8 +1,11 @@
 package com.mycheck.lchecksum.model;
+
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import java.io.IOException;
+import java.util.HashMap;
 
 public class RawJson {
     
@@ -19,8 +22,15 @@ public class RawJson {
         return name;
     }
 
-    public String toJsonString() throws JsonProcessingException {
-        ObjectMapper objectMapper = new ObjectMapper();
-        return objectMapper.writeValueAsString(this);
+    public HashMap<String, Object> toHashMap() {
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            String json = objectMapper.writeValueAsString(this);
+            ObjectMapper objectMapper2 = new ObjectMapper();
+            return objectMapper2.readValue(json, new TypeReference<HashMap<String, Object>>() {});
+        } catch (IOException e) {
+            e.printStackTrace(); 
+            return new HashMap<>();
+        }
     }
 }
