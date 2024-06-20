fe2d55eb7b215d8c11c60af9447940ad0d9967a3
lorensdima <laurencedimalanta@gmail.com>
2024-06-20 20:24:28 +0800

    add validate api route to validate input json

--- src/main/java/com/mycheck/lchecksum/controller/ConversionController.java
+++ src/main/java/com/mycheck/lchecksum/controller/ConversionController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.mycheck.lchecksum.exceptions.InvalidChecksumValue;
 import com.mycheck.lchecksum.model.RawJson;
 import com.mycheck.lchecksum.service.ConversionService;
 
@@ -27,4 +28,37 @@ public class ConversionController {
         
         return jsonMap;
     }
+
+    @PostMapping("/validate")
+    public HashMap<String, String> postMethodName(@RequestBody RawJson obj) {
+        HashMap<String, String> resultMap = new HashMap<>();
+        try {
+            HashMap<String, Object> jsonMap = new HashMap<>();
+            jsonMap = obj.toHashMap();
+
+            String checksum = jsonMap.get("checksum").toString();
+            if (checksum.length() != 3) {
+                throw new InvalidChecksumValue("Checksum length is not equal to 3");
+            }
+            String lcheckSumVal = conversionService.computeCheckSum(jsonMap);
+
+
+            if (checksum.equals(lcheckSumVal)) {
+                resultMap.put("result", "true");
+                resultMap.put("description", "No data has been lost");
+            } else {
+                resultMap.put("result", "false");
+                resultMap.put("description", "Checksum does not match. Highly likely that some data have been lost");
+            }
+            resultMap.put("error", "false");
+            
+        } catch (Exception e) {
+            resultMap.put("result", "false");
+            resultMap.put("error", "true");
+            resultMap.put("description", "Error ocurred: " + e.getMessage());
+        }
+        
+        return resultMap;
+    }
+    
 }
